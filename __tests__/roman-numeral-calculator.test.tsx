30206b0cf082d2276d6f311863f3b0866aaf9929
Qamar <qazam@lucidya.com>
2023-10-24 15:55:36 +0300

    fix test cases

--- __tests__/roman-numeral-calculator.test.tsx
+++ __tests__/roman-numeral-calculator.test.tsx
@@ -1,4 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
+//import '@testing-library/jest-dom';
+
 import { render, screen, fireEvent } from '@testing-library/react';
+import RomanNumeralCalculator from '../app/components/roman-numeral-calculator';
 
 describe('Roman Numeral Calculator', () => {
   it('should render the basic fields', () => {
@@ -6,7 +13,7 @@ describe('Roman Numeral Calculator', () => {
 
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     expect(
-      screen.getByRole('button', { name: /Calculate/i })
+      screen.getByRole('button', { name: /Convert/i })
     ).toBeInTheDocument();
   });
 
@@ -15,7 +22,7 @@ describe('Roman Numeral Calculator', () => {
 
     const input = screen.getByRole('textbox');
     fireEvent.change(input, { target: { value: '-3' } });
-    fireEvent.click(screen.getByRole('button', { name: /Calculate/i }));
+    fireEvent.click(screen.getByRole('button', { name: /Convert/i }));
 
     expect(
       screen.getByText(/Please enter non negative integers only./i)
@@ -27,7 +34,7 @@ describe('Roman Numeral Calculator', () => {
 
     const input = screen.getByRole('textbox');
     fireEvent.change(input, { target: { value: '3000' } });
-    fireEvent.click(screen.getByText('Calculate'));
+    fireEvent.click(screen.getByText('Convert'));
 
     expect(
       screen.getByText(/Please enter number between 1 to 1000./i)
@@ -39,7 +46,7 @@ describe('Roman Numeral Calculator', () => {
 
     const input = screen.getByRole('textbox');
     fireEvent.change(input, { target: { value: '0' } });
-    fireEvent.click(screen.getByText('Calculate'));
+    fireEvent.click(screen.getByText('Convert'));
 
     expect(screen.getByText('Nulla')).toBeInTheDocument();
   });
@@ -49,7 +56,7 @@ describe('Roman Numeral Calculator', () => {
 
     const input = screen.getByRole('textbox');
     fireEvent.change(input, { target: { value: '25' } });
-    fireEvent.click(screen.getByText('Calculate'));
+    fireEvent.click(screen.getByText('Convert'));
 
     expect(screen.getByText('XXV')).toBeInTheDocument();
   });
